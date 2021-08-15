f58dfc36b5bc23edac0c265f941f5e13e2de5110
Adam Stohl <iohzrd@gmail.com>
2021-08-15 14:41:35 -0700

    wait for ipfs...

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,14 +6,12 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
-#![allow(
-    // Clippy bug: https://github.com/rust-lang/rust-clippy/issues/7422
-    clippy::nonstandard_macro_braces,
-)]
 
-use tauri::{
-  api::process::{Command},
-};
+// use ipfs_api::IpfsClient;
+use std::{thread, time::Duration};
+use tauri::api::process::Command;
+
+use ipfs_api::IpfsClient;
 
 fn main() {
   tauri::Builder::default()
@@ -23,7 +21,7 @@ fn main() {
         match launch_ipfs_daemon().await {
           Ok(()) => (),
           Err(_err) => {
-            // log::error!("There was an error launching holochain: {:?}", err);
+            // log::error!("There was an error launching ipfs: {:?}", err);
           }
         }
         // log::info!("Launch setup successful")
@@ -47,5 +45,42 @@ async fn launch_ipfs_daemon() -> Result<(), String> {
     ])
     .spawn()
     .map_err(|err| format!("Failed to execute ipfs: {:?}", err))?;
+
+  let client = IpfsClient::default();
+  match wait_for_ipfs_ready().await {
+    Ok(connected) => eprintln!("connected: {:?}", connected),
+    Err(e) => eprintln!("error waiting for ipfs: {}", e),
+  }
+
+  match client.id(None).await {
+    Ok(id) => eprintln!("id: {:?}", id.id),
+    Err(e) => eprintln!("error getting id: {}", e),
+  }
+
   Ok(())
+}
+
+async fn wait_for_ipfs_ready() -> Result<String, String> {
+  let client = IpfsClient::default();
+  // A counter variable
+  let mut iden = "".to_string();
+  let mut retries = 1;
+  let mut connected = false;
+  // Loop while `n` is less than 101
+  while !connected && retries < 300 {
+    println!("trying to connect to ipfs");
+    match client.id(None).await {
+      Ok(id) => {
+        connected = true;
+        iden = id.id;
+      }
+      Err(e) => {
+        retries += 1;
+        eprintln!("connected: {:?}, {:?}", connected, e);
+        thread::sleep(Duration::from_millis(100));
+      }
+    }
+  }
+
+  Ok(iden)
 }
