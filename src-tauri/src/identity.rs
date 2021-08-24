7c2beaec1b22d53b6046ccad4ba1d2d2b04986d8
Adam Stohl <iohzrd@gmail.com>
2021-08-23 20:37:52 -0700

    db stuff

--- src-tauri/src/identity.rs
+++ src-tauri/src/identity.rs
@@ -1,19 +1,13 @@
-// use std::io::{self, Write};
-// use std::str::from_utf8;
-// use std::time::SystemTime;
-use chrono::prelude::{DateTime, Utc};
+use chrono::{offset::Utc, DateTime, NaiveDate};
 use futures::TryStreamExt;
 use ipfs_api::IpfsClient;
-use rusqlite::types::Value;
-use rusqlite::vtab::array;
-use rusqlite::{params, Connection, Result, NO_PARAMS};
+use rusqlite::{params, Connection, Result};
 use rusqlite_migration::{Migrations, M};
-use serde_rusqlite::*;
-use std::rc::Rc;
-use std::str::FromStr;
 use std::{thread, time::Duration};
 use tauri::api::process::Command;
 use tauri::command;
+
+use crate::identity::types::Identity;
 
 pub mod types;
 
@@ -29,16 +23,16 @@ pub fn request_test_identity() -> types::Identity {
     value: "1T8mM7TDWBcxKF5ZZy7B58adMsBgxivr1".to_string(),
   };
   let test_identity_object = types::Identity {
-    aux: vec![test_aux_object],
+    aux: serde_json::json!([test_aux_object]),
     av: "".to_string(),
     dn: "iohzrd".to_string(),
-    following: vec![
-      "12D3KooWDED1CudLX9sdi1qBzy5tHS4Xi2Mpk45E5wrqteri1R8z".to_string(),
+    following: serde_json::json!([
+      "12D3KooWDED1CudLX9sdi1qBzy5tHS4Xi2Mpk45E5wrqteri1R8z",
       "Qmb4zrL17TtLGnaLFuUQC4TmaVbizEfVbDnnSzNLxkZ3Zp".to_string(),
       "12D3KooWJd8q6Q9seVVA8HmSjqBN13kZaVZd4GtMxFha3VqiGfG9".to_string(),
-    ],
-    meta: vec![],
-    posts: vec![
+    ]),
+    meta: serde_json::json!([]),
+    posts: serde_json::json!([
       "QmcoD56GRcE3eZZ3sFi91Ym98ZhvALxdtgPWmydRfX3sFx".to_string(),
       "QmQXHdVMMi45tDUEfNuaXMBbyeP1cAWjH7pf5V8ZPFStuj".to_string(),
       "QmRvi98hynA4qetsb9MG1HDQcLad8x9MNmGpqQFNBz59Bd".to_string(),
@@ -53,7 +47,7 @@ pub fn request_test_identity() -> types::Identity {
       "QmWEvWXpcp9JSZ2taLhY8eby2CaBUTAojpXtJsfTab11Bh".to_string(),
       "QmNtN4TVvx2XL3f1BNB4v5wH5yeDwrNkZap9a1r6F6KQBM".to_string(),
       "QmQW72f51MRFj9PaJnLPcUWkZXRMcQVctf1ExJXrU3wWRs".to_string(),
-    ],
+    ]),
     publisher: "12D3KooWDED1CudLX9sdi1qBzy5tHS4Xi2Mpk45E5wrqteri1R8z".to_string(),
     ts: DateTime::timestamp(&Utc::now()),
   };
@@ -116,65 +110,95 @@ pub async fn ipfs_id() -> String {
 //   "iden".into()
 // }
 
-pub async fn initialize_database(publisher: String) -> Result<()> {
-  //   let client = IpfsClient::default();
-  //   let mut publisher = "".to_string();
-  //   match get_ipfs_id(&client).await {
-  //     Ok(id) => publisher = id,
-  //     Err(e) => eprintln!("There was an error launching ipfs: {:?}", e),
-  //   }
+pub async fn initialize_database(publisher: &String) -> Result<()> {
+  println!("initialize_database: {:?}", &publisher);
+
+  // let client = IpfsClient::default();
+  // let mut publisher = "".to_string();
+  // match get_ipfs_id(&client).await {
+  //   Ok(id) => publisher = id,
+  //   Err(e) => eprintln!("There was an error launching ipfs: {:?}", e),
+  // }
+
+  let mut conn = Connection::open("test.db")?;
 
   let create_identity_table = "create table if not exists identity (
       id          integer primary key,
-      aux         text not null,
-      av          text not null,
-      dn          text not null,
-      following   text not null,
-      meta        text not null,
-      posts       text not null,
-      publisher   text not null,
+      aux         text,
+      av          text,
+      dn          text,
+      following   text,
+      meta        text,
+      posts       text,
+      publisher   text,
       ts          int
     )";
 
   let create_post_table = "create table if not exists identity (
-      id          integer primary key,
-      aux         text not null,
-      av          text not null,
-      dn          text not null,
-      following   text not null,
-      meta        text not null,
-      posts       text not null,
-      publisher   text not null,
-      ts          int
-    )";
+        id          integer primary key,
+        aux         text not null,
+        av          text not null,
+        dn          text not null,
+        following   text not null,
+        meta        text not null,
+        posts       text not null,
+        publisher   text not null,
+        ts          int
+      )";
 
-  // 1️⃣ Define migrations
   let migrations = Migrations::new(vec![
     M::up(create_identity_table),
     M::up(create_post_table),
-    // In the future, add more migrations here:
-    //M::up("ALTER TABLE friend ADD COLUMN email TEXT;"),
+    // In the future, add more migrations here
   ]);
 
-  let mut conn = Connection::open(publisher.clone() + ".db").unwrap();
-  array::load_module(&conn)?;
-
-  // Apply some PRAGMA, often better to do it outside of migrations
-  conn.pragma_update(None, "journal_mode", &"WAL").unwrap();
-
-  // 2️⃣ Update the database schema, atomically
   migrations.to_latest(&mut conn).unwrap();
 
-  let me = types::Identity {
-    aux: vec![],
-    av: "".to_string(),
-    dn: "".to_string(),
-    following: vec![publisher.clone()],
-    meta: vec![],
-    posts: vec![],
-    publisher: publisher.clone(),
-    ts: DateTime::timestamp(&Utc::now()),
-  };
+  let mut stmt = conn.prepare(
+    "SELECT aux,av,dn,following,meta,posts,publisher,ts FROM identity where publisher = ? LIMIT 1",
+  )?;
+  let identities = stmt.query_map(params![&publisher], |row| {
+    Ok(Identity {
+      aux: row.get(0)?,
+      av: row.get(1)?,
+      dn: row.get(2)?,
+      following: row.get(3)?,
+      meta: row.get(4)?,
+      posts: row.get(5)?,
+      publisher: row.get(6)?,
+      ts: row.get(7)?,
+    })
+  })?;
+
+  //   println!("identities {:?}", &identities.);
+
+  for identity in identities {
+    println!("Found person {:?}", identity.unwrap());
+  }
+
+  // let me = types::Identity {
+  //     aux: serde_json::json!([]),
+  //     av: "".to_string(),
+  //     dn: "".to_string(),
+  //     following: serde_json::json!(["12D3KooWDED1CudLX9sdi1qBzy5tHS4Xi2Mpk45E5wrqteri1R8z"]),
+  //     meta: serde_json::json!([]),
+  //     posts: serde_json::json!([]),
+  //     publisher: "12D3KooWDED1CudLX9sdi1qBzy5tHS4Xi2Mpk45E5wrqteri1R8z".to_string(),
+  //     ts: DateTime::timestamp(&Utc::now()),
+  //   };
+  //   conn.execute(
+  //     "INSERT INTO identity (aux,av,dn,following,meta,posts,publisher,ts) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
+  //     params![
+  //         me.aux,
+  //         me.av,
+  //         me.dn,
+  //         me.following,
+  //         me.meta,
+  //         me.posts,
+  //         me.publisher,
+  //         me.ts,
+  //     ],
+  // )?;
 
   Ok(())
 }
