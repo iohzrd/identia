c56d1663c8535c4968df4cc2c794b357ecaf9358
Adam Stohl <iohzrd@gmail.com>
2021-10-20 17:17:03 -0700

    assets 'n stuff

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-  import type { Post, PostResponse, MediaResponse } from "../types.type";
+  import type { PostResponse, MediaResponse } from "../types.type";
   import { Buffer } from "buffer/index";
   import { ClickableTile } from "carbon-components-svelte";
   import { Link } from "carbon-components-svelte";
   // import { Player, Video, DefaultUi } from "@vime/svelte";
   import { invoke } from "@tauri-apps/api/tauri";
   import { onMount, onDestroy } from "svelte";
+  // import { create } from "ipfs-http-client";
 
   // let player: Player;
   export let cid: String;
@@ -15,26 +16,14 @@
 
   async function getPostFromCid() {
     console.log("getPostFromCid");
-    await invoke("get_post_ipfs", {
+    postResponse = await invoke("get_post_ipfs", {
       cid: cid,
-    })
-      .then((pr: PostResponse) => {
-        console.log(pr);
-        if (pr && pr.cid && pr.post) {
-          postResponse = pr;
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    });
   }
 
   async function getFile(filename) {
-    console.log("getFile");
     const root_cid = postResponse.cid || cid;
     const path = root_cid + "/" + filename;
-    console.log("path");
-    console.log(path);
     const mr: MediaResponse = await invoke("get_file_ipfs", {
       cid: path,
     });
@@ -46,6 +35,26 @@
       mime: mr.mime,
     };
   }
+
+  // async function getFile(filename) {
+  //   // this might be more efficient but,
+  //   // doesn't work because tauri CORS bug...
+  //   let bufs = [];
+  //   const root_cid = postResponse.cid || cid;
+  //   const path = root_cid + "/" + filename;
+  //   const ipfs = await create("/ip4/127.0.0.1/tcp/5001");
+  //   for await (const buf of ipfs.cat(path)) {
+  //     bufs.push(buf);
+  //   }
+  //   const buf = Buffer.concat(bufs);
+  //   // get mime here...
+  //   const blob = new Blob([buf], { type: "image/png" });
+  //   const urlCreator = window.URL || window.webkitURL;
+  //   return {
+  //     blob: urlCreator.createObjectURL(blob),
+  //     mime: "image/png",
+  //   };
+  // }
 
   onMount(async () => {
     if (!postResponse) {
