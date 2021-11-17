31c3f480e84b6ee7fa7cfe6afcc534c246651a95
Adam Stohl <iohzrd@gmail.com>
2021-11-17 08:33:56 -0800

    mediaModal

--- src/components/Identity.svelte
+++ src/components/Identity.svelte
@@ -16,8 +16,9 @@
   import { invoke } from "@tauri-apps/api/tauri";
   import { onMount, onDestroy } from "svelte";
 
-  import Post from "./Post.svelte";
+  import MediaModal from "./MediaModal.svelte";
   import Meta from "./Meta.svelte";
+  import Post from "./Post.svelte";
   import type { Identity, IdentityResponse, PostResponse } from "../types.type";
 
   export let params = {};
@@ -30,6 +31,9 @@
   let posts_limit: number = 5;
   $: publisher = params["publisher"];
   $: posts_query = `SELECT posts.cid, posts.body, posts.files, posts.meta, posts.publisher, posts.timestamp, identities.display_name FROM posts INNER JOIN identities ON identities.publisher = posts.publisher WHERE posts.publisher = '${publisher}' AND posts.timestamp < ${posts_oldest_ts} ORDER BY posts.timestamp DESC LIMIT ${posts_limit}`;
+
+  let media_modal_open = false;
+  let media_modal_media = [];
 
   async function getPostsPage() {
     console.log(`getFeedPage: ${publisher}`);
@@ -89,6 +93,8 @@
     params = {};
   });
 </script>
+
+<MediaModal bind:media_modal_media bind:media_modal_open />
 
 <Form on:submit>
   {#if identity}
@@ -155,7 +161,12 @@
       {#if identity && identity.posts}
         {#each posts as post_response}
           <div>
-            <Post cid={null} {post_response} {setMediaModal} />
+            <Post
+              cid={null}
+              {post_response}
+              bind:media_modal_media
+              bind:media_modal_open
+            />
           </div>
         {/each}
       {/if}
