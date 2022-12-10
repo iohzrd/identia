d8870a0b43f45548f0fc425cf4dc71e5ff5cf3fc
iohzrd <iohzrd@gmail.com>
2022-12-10 10:23:26 -0800

    deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
 import sveltePreprocess from "svelte-preprocess";
-import { optimizeImports } from "carbon-preprocess-svelte";
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [sveltePreprocess(), optimizeImports()],
+  preprocess: [sveltePreprocess()],
 };
