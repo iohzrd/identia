98914f092dc7bab58a12fa5a4372a78f9fa08deb
iohzrd <iohzrd@gmail.com>
2023-12-25 11:19:36 -0800

    deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
